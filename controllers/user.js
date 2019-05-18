d6a1f0aa6988927fdcf886145de3d8be3f2b9691
ade rahmanp <aderahmanp@gmail.com>
2019-05-18 22:18:53 +0700

    sign-in

--- controllers/user.js
+++ controllers/user.js
@@ -23,3 +23,49 @@ exports.signUp = function (req, res) {
       })
     )
 }
+
+exports.signIn = (req, res) => {
+  User.findOne({
+    $or: [{ username: req.body.username }, { email: req.body.email }]
+  })
+    .exec()
+    .then(function (userInfo) {
+      if (!userInfo) {
+        return res.status(400).json({
+          success: false,
+          message: 'username or password in incorrect'
+        })
+      }
+      const valid = bcrypt.compareSync(req.body.password, userInfo.password)
+      if (valid) {
+        const payload = {
+          id: userInfo.id,
+          email: userInfo.email,
+          username: userInfo.username
+        }
+        var token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+          algorithm: 'HS256',
+          expiresIn: 86400
+        })
+        return res.status(200).json({
+          success: true,
+          message: 'Signed-in successfully',
+          data: {
+            token: token,
+            name: userInfo.name
+          }
+        })
+      } else {
+        res.status(403).json({
+          success: false,
+          message: 'username or passsword in incorrect'
+        })
+      }
+    })
+    .catch(err => {
+      return res.status(400).json({
+        success: false,
+        message: err.message
+      })
+    })
+}
