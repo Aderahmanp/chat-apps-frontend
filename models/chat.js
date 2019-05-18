5bb0fae27a902f3e3217e61b4c7ff019f04103c1
ade rahmanp <aderahmanp@gmail.com>
2019-05-19 01:14:49 +0700

    add from & to

--- models/chat.js
+++ models/chat.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const chatSchema = new Schema({
-  massage: { type: String },
-  from: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  to: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  message: { type: String },
+  from: { type: Schema.Types.ObjectId, ref: 'User' },
+  to: { type: Schema.Types.ObjectId, ref: 'User' },
   date: { type: Date, default: Date.now }
 })
 
