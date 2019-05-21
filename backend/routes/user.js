9417b26126ab86850a445d2e8063c1f87c9b47f5
ade rahmanp <aderahmanp@gmail.com>
2019-05-22 00:58:58 +0700

    get Me from token

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const router = express.Router()
-const userController = require('../controllers/user')
-const auth = require('../middleware/jwtAuth')
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user");
+const auth = require("../middleware/jwtAuth");
 
-router.post('/sign-up/', userController.signUp)
+router.post("/sign-up/", userController.signUp);
 
-router.post('/sign-in', userController.signIn)
+router.post("/sign-in", userController.signIn);
 
-module.exports = router
+router.get("/me", auth.isAuthenticated, userController.Me);
+
+module.exports = router;
